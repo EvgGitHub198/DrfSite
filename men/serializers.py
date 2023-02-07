a42d72068e81896331689090899e752788773cc1
Evg <mishagin02@mail.ru>
2023-02-07 21:14:00 +0300

    init

---
+++ men/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+
+from .models import Men
+
+
+class MenSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Men
+        fields = ('title', 'cat_id')
