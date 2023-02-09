24d7018e866e0e99dfd010ab3897a13e50ff64ce
Evg <mishagin02@mail.ru>
2023-02-09 21:13:18 +0300

    last

--- men/serializers.py
+++ men/serializers.py
@@ -1,9 +1,13 @@
 from rest_framework import serializers
+from rest_framework.parsers import JSONParser
+from rest_framework.renderers import JSONRenderer
 
 from .models import Men
 
 
 class MenSerializer(serializers.ModelSerializer):
+    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
     class Meta:
         model = Men
-        fields = ('title', 'cat_id')
+        fields = "__all__"
+
--- men/views.py
+++ men/views.py
@@ -1,27 +1,54 @@
+from _ast import Is
+
 from django.forms import model_to_dict
 from django.shortcuts import render
-from rest_framework import generics
+from django.views import generic
+from rest_framework import generics, viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from .permissions import IsAdminOrReadOnly, IsOwnerOrReadOnly
 from .serializers import MenSerializer
-from .models import Men
+from .models import Men, Category
 
 
-class MenAPIView(APIView):
-     def get(self, request):
-          lst = Men.objects.all().values()
-          return Response({'posts': list(lst)})
-     def post(self, request):
-          post_new = Men.objects.create(
-               title=request.data['title'],
-               content=request.data['content'],
-               cat_id=request.data['cat_id']
-          )
-          return Response({'post': model_to_dict(post_new)})
-
-
-# class MenAPIView(generics.ListAPIView):
+# class MenViewSet(viewsets.ModelViewSet):
 #      queryset = Men.objects.all()
 #      serializer_class = MenSerializer
+#
+#      def get_queryset(self):
+#           pk = self.kwargs.get('pk')
+#           if not pk:
+#                return Men.objects.all()[:3]
+#           return Men.objects.filter(pk=pk)
+#
+#
+#      @action(methods=['get'], detail=True)
+#      def category(self, request, pk=None):
+#           cats = Category.objects.get(pk=pk)
+#           return Response({'cats': cats.name})
+
+
+class MenAPIList(generics.ListCreateAPIView):
+     queryset = Men.objects.all()
+     serializer_class = MenSerializer
+     permission_classes = (IsAuthenticatedOrReadOnly, )
+
+
+class MenAPIUpdate(generics.UpdateAPIView):
+     queryset = Men.objects.all()
+     serializer_class = MenSerializer
+     permission_classes = (IsAuthenticated, )
+     # authentication_classes = (TokenAuthentication, )
+
+class MenAPIDestroy(generics.RetrieveDestroyAPIView):
+     queryset = Men.objects.all()
+     serializer_class = MenSerializer
+     permission_classes = (IsAdminOrReadOnly, )
+
+
 
